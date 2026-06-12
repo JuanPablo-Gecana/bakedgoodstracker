b485abbf0d091e3b2336238036a29b001cf57558
JP Gecana <jprgecana@gmail.com>
2026-06-12 19:55:57 +0800

    Update panaderyanihuwanpablo.cpp
    Dagdag comments troughout the code para maintindihan hehe

--- panaderyanihuwanpablo.cpp
+++ panaderyanihuwanpablo.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <iomanip> //Might use para sa generation ng recipt ng customer or baka sa design ng program itself
-#include <string>
-#include <vector>
-#include <fstream> //pang save ng inventory sa file
+#include <string> //Personally di ko pa nakikita relevancy neto pero itetest ko pa
+#include <vector> //Needed to para makapagstore ng as many products as we can without limitations
+#include <fstream> //pang save ng inventory sa file and possibly ng transaction history ng bawat customers
 using namespace std;
 
+//Structure para malagay natin ung mga products and info nila sa vector as one 
 struct bakedProcuct {
     string name;
     double price;
     int quantity;
 };
 
-vector <bakedProcuct> menu; //pang lagay ng mga items na ibebenta
+vector <bakedProcuct> menu; //pang lagay ng mga items na ibebenta and ididisplay later sa display function
 
 //Dito lahat ilalagay ung mga products na ibebenta and ung mga relevant info nila
 void addProduct() {
@@ -22,6 +23,8 @@ void addProduct() {
     cin >> newProduct.price;
     cout << "Enter the quantity of the product: ";
     cin >> newProduct.quantity;
+    /* Once tapos na malagyan ung product and ung info nila, ipupush back na dun sa vector and magiging item na sya ng tracker natin
+    and pwede na sya makita and maorder ng magiging customer*/
     menu.push_back(newProduct);
     cout << "Product added successfully!" << endl;
 }
@@ -37,8 +40,9 @@ void displayMenu() {
 
 int main() {
     int choice;
+    //Do-while para paulit ulit and para wala nang initialization ng value ung choice variable
     do {
-        cout << "========PANADEYA NI HUWAN PABLO========" << endl
+        cout << "========PANADEYA NI HUWAN PABLO========" << endl //Papaltan yang "PANADERYA NI HUWAN PABLO" kase di pwede yan HAHAHA
              << "1. Add Product" << endl
              << "2. Display All Products" << endl
              << "3. Exit" << endl
